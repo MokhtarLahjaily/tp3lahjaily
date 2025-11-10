ad626bd430e3559656ceb6044b7fa48e71718a62
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-11-10 18:44:43 +0100

    etat initial

---
+++ src/main/java/ma/emsi/lahjaily/tp3lahjaily/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.lahjaily.tp3lahjaily;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
